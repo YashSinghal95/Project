18fa50d0f8f97109177611d49c3a020c0ed4dc3d
YashSinghal95 <glau.cs22@gmail.com>
2023-09-07 23:15:08 +0530

    Intro changed

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function homepage() {
   return (
     <div className='home'>
-      <h1>" this is a website for hospital management. This will contain few Lines here. for example "</h1>
+      <h1>" Welcome to our Hospital Management System! We're dedicated to enhancing healthcare administration, making it more efficient and effective. Our user-friendly platform simplifies tasks, streamlines processes, and ensures seamless communication between healthcare providers, staff, and patients. Discover the future of healthcare management with us. "</h1>
       <Link to='/login'><button>Login</button></Link>
       <Link to='/regester'><button>Register</button></Link>
         
